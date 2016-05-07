a7acfe8f749d9a9d05c62cb3e887a06c2f322550
Jonas Nicklas <jonas.nicklas@gmail.com>
2016-05-08 00:30:19 +0200

    Add test

--- tests/basic.rs
+++ tests/basic.rs
@@ -34,3 +34,39 @@ fn test_basic_query() {
     assert_eq!(person.find("tags"), Some(&Value::Array(vec![Value::String(String::from("foo")), Value::String(String::from("bar"))])));
     assert_eq!(person.find("age"), None);
 }
+
+#[test]
+fn test_query_with_fragments() {
+    let doc = "
+        query {
+            tagged(id: [\"foo\"]) {
+                id,
+                tags,
+
+                ... on Post {
+                    title
+                }
+
+                ... Name
+            }
+        }
+
+        fragment Name on Person {
+            first_name,
+            last_name,
+        }
+    ";
+    let context = graphers::parse(doc);
+    let query = context.query().expect("should define a query");
+
+    let result = serde_json::to_string(&query::query(query)).expect("failed to serialize");
+
+    let value: Value = serde_json::from_str(&result).expect("should generate valid JSON");
+
+    let person = value.find("person").expect("should have person in output");
+
+    assert_eq!(person.find("first_name"), Some(&Value::String(String::from("Jonas"))));
+    assert_eq!(person.find("last_name"), Some(&Value::String(String::from("Nicklas"))));
+    assert_eq!(person.find("tags"), Some(&Value::Array(vec![Value::String(String::from("foo")), Value::String(String::from("bar"))])));
+    assert_eq!(person.find("age"), None);
+}

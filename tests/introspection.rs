1eb930da192273156d84c72003a19356c8740920
Jonas Nicklas <jonas.nicklas@gmail.com>
2016-08-12 16:03:08 +0200

    Add type name introspection

--- tests/introspection.rs
+++ tests/introspection.rs
@@ -19,6 +19,33 @@ fn test_direct_schema_introspection() {
 }
 
 #[test]
+fn test_reflect_on_type_name_on_interface() {
+    let doc = "
+        query {
+            tagged(id: [\"foo\"]) {
+                id,
+                tags,
+                __typename
+            }
+        }
+    ";
+    let context = graphers::parse(doc);
+
+    let result = serde_json::to_string(&Schema.query(&context)).expect("failed to serialize");
+
+    let value: Value = serde_json::from_str(&result).expect("should generate valid JSON");
+
+    let tagged = value.find("tagged").expect("should have tagged data in output").as_array().expect("should be an array");
+
+    assert_eq!(tagged[0].find("id"), Some(&Value::String(String::from("6543"))));
+    assert_eq!(tagged[1].find("id"), Some(&Value::String(String::from("9876"))));
+    assert_eq!(tagged[0].find("tags"), Some(&Value::Array(vec![Value::String(String::from("foo")), Value::String(String::from("bar"))])));
+    assert_eq!(tagged[1].find("tags"), Some(&Value::Array(vec![Value::String(String::from("GraphQL")), Value::String(String::from("Rust"))])));
+    assert_eq!(tagged[0].find("__typename"), Some(&Value::String(String::from("Person"))));
+    assert_eq!(tagged[1].find("__typename"), Some(&Value::String(String::from("Post"))));
+}
+
+#[test]
 fn test_reflect_on_type_by_name() {
     let doc = "
         query {

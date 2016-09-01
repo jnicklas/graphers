47550f264f2c8b855258b25edd6d71b88cc5c098
Jonas Nicklas <jonas.nicklas@gmail.com>
2016-09-01 10:25:30 +0200

    Allow shorthand top level queries, closes #19

--- graphers_parse/tests/query.rs
+++ graphers_parse/tests/query.rs
@@ -35,3 +35,36 @@ fn test_basic_query() {
     assert_eq!(friend_field.arguments()[0].name().as_str(), "name");
     assert_eq!(friend_field.arguments()[0].value(), &Value::String("Mark Zuckerberg".into()));
 }
+
+#[test]
+fn test_basic_shorthand_query() {
+    let context = parse::parse("
+        {
+            person(id: \"1\") {
+                first_name,
+                zuck: friend(name: \"Mark Zuckerberg\")
+            }
+        }
+    ").expect("should be valid");
+
+    let query = context.query().expect("there should be a query");
+
+    let person_field = query.selection_set().get(0).and_then(|s| s.field()).expect("should have a person field");
+
+    let first_name_field = person_field.selection_set().get(0).and_then(|s| s.field()).expect("should have a first name field");
+    let friend_field = person_field.selection_set().get(1).and_then(|s| s.field()).expect("should have a friend field");
+
+    assert_eq!(person_field.name().as_str(), "person");
+    assert_eq!(first_name_field.name().as_str(), "first_name");
+    assert_eq!(friend_field.name().as_str(), "friend");
+
+    assert_eq!(person_field.alias().as_str(), "person");
+    assert_eq!(first_name_field.alias().as_str(), "first_name");
+    assert_eq!(friend_field.alias().as_str(), "zuck");
+
+    assert_eq!(person_field.arguments()[0].name().as_str(), "id");
+    assert_eq!(person_field.arguments()[0].value(), &Value::String("1".into()));
+    assert_eq!(first_name_field.arguments().len(), 0);
+    assert_eq!(friend_field.arguments()[0].name().as_str(), "name");
+    assert_eq!(friend_field.arguments()[0].value(), &Value::String("Mark Zuckerberg".into()));
+}

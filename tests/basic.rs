2b2aedced77e14d6b578676f4358c2b7802ce784
Jonas Nicklas <jonas.nicklas@gmail.com>
2016-09-06 17:06:33 +0200

    Extract argument tests

--- tests/basic.rs
+++ tests/basic.rs
@@ -35,34 +35,6 @@ fn test_basic_query() {
 }
 
 #[test]
-fn test_missing_required_argument() {
-    let doc = "
-        query {
-            person { first_name, }
-        }
-    ";
-    let context = graphers::parse(doc).expect("should be valid");
-
-    let result = serde_json::to_string(&Schema.query(&context)).unwrap_err();
-
-    assert_eq!(format!("{}", result), "MissingArgument: missing required argument id at line 0 column 0");
-}
-
-#[test]
-fn test_wrong_type_of_argument() {
-    let doc = "
-        query {
-            person(id: 123.33) { first_name, }
-        }
-    ";
-    let context = graphers::parse(doc).expect("should be valid");
-
-    let result = serde_json::to_string(&Schema.query(&context)).unwrap_err();
-
-    assert_eq!(format!("{}", result), "CoercionError: cannot coerce the value Float(123.33) to String at line 0 column 0");
-}
-
-#[test]
 fn test_custom_scalars() {
     let doc = "
         query {
